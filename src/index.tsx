0ace423be943b847e1f6276c031a3bca21a7867a
Serge <serge.olabin@gmail.com>
2019-09-06 14:17:51 +0300

    Added handler

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,92 @@
-/**
- * @class ExampleComponent
- */
+type keyCodeType = number | string;
+type functionType = (...args: any[]) => any;
+interface IEventHandlerElem {
+  eventType: string;
+  listener: functionType;
+}
 
-import * as React from 'react'
+class KeypressHandler {
+  private intervalID!: NodeJS.Timer;
+  private interval!: number;
+  private watchingDict: {
+    [key: string]: {
+      handler: functionType;
+      isRunning: boolean;
+      onHoldDependent?: boolean;
+    };
+  } = {};
+  private eventListenersList: IEventHandlerElem[] = [];
 
-import styles from './styles.css'
+  private tick() {
+    for (const keyKode in this.watchingDict) {
+      const watcher = this.watchingDict[keyKode];
+      if (watcher.onHoldDependent && watcher.isRunning) watcher.handler();
+    }
+  }
 
-export type Props = { text: string }
+  private removeEventListeners() {
+    this.eventListenersList.forEach(eventListener => {
+      document.removeEventListener(
+        eventListener.eventType,
+        eventListener.listener,
+        false,
+      );
+    });
+  }
 
-export default class ExampleComponent extends React.Component<Props> {
-  render() {
-    const {
-      text
-    } = this.props
+  public run(interval?: number) {
+    this.interval = interval || 100;
+    this.intervalID = setInterval(() => this.tick(), this.interval);
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+    const keydownChecker = (event: KeyboardEvent) => {
+      const key = +event.keyCode;
+      const watcher = this.watchingDict[key];
+
+      if (!watcher || watcher.isRunning) return;
+
+      if (this.intervalID) clearInterval(this.intervalID);
+      watcher.handler();
+      watcher.isRunning = true;
+      this.intervalID = setInterval(() => this.tick(), 100);
+    };
+    const keyupChecker = (event: KeyboardEvent) => {
+      const key = +event.keyCode;
+      const watcher = this.watchingDict[key];
+
+      if (!watcher) return;
+      watcher.isRunning = false;
+    };
+
+    document.addEventListener('keydown', keydownChecker, false);
+    document.addEventListener('keyup', keyupChecker, false);
+
+    this.eventListenersList.push({
+      eventType: 'keydown',
+      listener: keydownChecker,
+    });
+    this.eventListenersList.push({
+      eventType: 'keyup',
+      listener: keyupChecker,
+    });
+  }
+
+  public stop(opts?: { keepListening: boolean }) {
+    if (this.intervalID) clearInterval(this.intervalID);
+
+    if (opts && !opts.keepListening) {
+      this.watchingDict = {};
+      this.removeEventListeners();
+    }
+  }
+
+  public onKey(keyCode: keyCodeType, handler: functionType, opts?: { hold: true }) {
+    this.watchingDict[keyCode] = {
+      handler,
+      isRunning: false,
+      onHoldDependent: opts && opts.hold,
+    };
   }
 }
+
+const kh = new KeypressHandler();
+export default kh;
--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,5 @@
-import ExampleComponent from './'
-
-describe('ExampleComponent', () => {
+describe('Keypress handler', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
-  })
-})
+    expect(true).toBeTruthy();
+  });
+});
